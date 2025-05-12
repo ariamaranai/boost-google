71c05a8f6ea52302b58055ddbfae21a6d30b8374
ariamaranai <106607480+ariamaranai@users.noreply.github.com>
2025-05-12 15:42:54 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,15 @@
-HTMLBodyElement.prototype.appendChild = a =>
-  a.tagName != "SCRIPT" &&
-  document.body.insertBefore(a, null);
+Object.defineProperties(navigator, {
+  sendBeacon: {
+    value: () => 0
+  },
+  userAgent: {
+    value: ""
+  }
+});
+Object.defineProperty(Image.prototype, "src", {
+  set (a) {
+    a[0] != "/" &&
+    a.slice(10, 18) != "w.google" &&
+    this.setAttribute("src", a);
+  }
+});
